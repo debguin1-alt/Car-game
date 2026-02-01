5be54aea6f78f503aec4d5a551cea5d5dc628133
debguin1-alt <debguin1@gmail.com>
2026-02-01 12:17:46 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = "car-game-v5";
+const FILES = [
+  "index.html",
+  "style.css",
+  "script.js",
+  "images/car.png",
+  "images/traffic.png",
+  "images/road.png",
+  "sounds/crash.mp3",
+  "sounds/drive.mp3"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
